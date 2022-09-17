8ea08fe35ca060ea636d0df0cf509caee4d45c6a
mnl0609 <marinelarevalo05@gmail.com>
2022-09-17 13:36:33 +0800

    initiat commit

---
+++ exercise1.php
@@ -0,0 +1,13 @@
+<?php
+	echo "Twinkle, twinkle little star.";
+?>
+
+<br>
+<br>
+
+<?php
+	$first = "Twinkle ";
+	$second = "Star";
+
+	echo $first . $second
+?>
---
+++ exercise3.php
@@ -0,0 +1,75 @@
+<?php 
+	$sample = 8;
+
+
+	echo "Value now is " . $sample;
+?>
+
+<br>
+
+<?php 
+	$sample = 8;
+	$sample += 2;
+
+	echo "Add 2. Value now is " . $sample;
+?>
+
+<br>
+
+<?php 
+	$sample = 8;
+	$sample += 2;
+	$sample -= 4;
+
+	echo "Subtract 4. Value now is " . $sample;
+?>
+
+<br>
+
+<?php 
+	$sample = 8;
+	$sample += 2;
+	$sample -= 4;
+	$sample *= 5;
+
+	echo "Multiply by 5. Value now is " . $sample;
+?>
+
+<br>
+
+<?php 
+	$sample = 8;
+	$sample += 2;
+	$sample -= 4;
+	$sample *= 5;
+	$sample /= 3;
+
+	echo "Divide it by 3. Value now is " . $sample;
+?>
+
+<br>
+
+<?php 
+	$sample = 8;
+	$sample += 2;
+	$sample -= 4;
+	$sample *= 5;
+	$sample /= 3;
+	$sample ++;
+
+	echo "Increment value by 1. Value now is " . $sample;
+?>
+
+<br>
+
+<?php 
+	$sample = 8;
+	$sample += 2;
+	$sample -= 4;
+	$sample *= 5;
+	$sample /= 3;
+	$sample ++;
+	$sample --;
+
+	echo "Decrement value by 1. Value now is " . $sample;
+?>
